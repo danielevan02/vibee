0db5f1ae3dba0664ccc70305737b72f598574483
danielevan02 <danielevan454@gmail.com>
2025-06-13 17:25:44 +0700

    fixing types error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import DemoVideo from "@/components/demo-video";
 import ParallaxSection from "@/components/section/parallax-section";
 import WorldSection from "@/components/section/world-section";
 import { Button } from "@/components/ui/button";
 import ColourfulText from "@/components/ui/colourful-text";
 import { cn } from "@/lib/utils";
-import {
-  ChevronDown,
-} from "lucide-react";
+import { ChevronDown } from "lucide-react";
 import Link from "next/link";
 
 export default function LandingPage() {
@@ -15,7 +12,9 @@ export default function LandingPage() {
       <div className="relative flex items-center justify-center h-screen">
         <div className="w-[90%] xl:w-[70%] m-auto flex flex-col lg:flex-row items-center gap-5">
           <div className="flex flex-col justify-between h-full gap-5 items-center">
-            <p className="italic">"Share the Vibe. Live the Moment."</p>
+            <p className="italic">
+              &quot;Share the Vibe. Live the Moment.&quot;
+            </p>
             <h1 className="text-3xl md:text-5xl capitalize font-extrabold text-center">
               <ColourfulText text="VIBEE" /> The best social media app for gen-z
             </h1>
@@ -23,14 +22,17 @@ export default function LandingPage() {
               Vibee is where your real moments turn into real vibes. Share,
               connect, and vibe with your people—anytime, anywhere.
             </p>
-            <Link href='/home' className="rounded-full px-8 py-4 border border-primary text-primary uppercase tracking-widest hover:bg-primary hover:text-white transition">
+            <Link
+              href="/home"
+              className="rounded-full px-8 py-4 border border-primary text-primary uppercase tracking-widest hover:bg-primary hover:text-white transition"
+            >
               join vibee now
             </Link>
           </div>
 
           <div className="rounded-md overflow-clip shadow-lg border w-full h-full">
             <video autoPlay loop muted playsInline className="w-full h-full">
-              <source src="/video.mp4" type="video/mp4"/>
+              <source src="/video.mp4" type="video/mp4" />
             </video>
           </div>
         </div>
@@ -65,14 +67,20 @@ export default function LandingPage() {
         <div className="relative z-20 flex flex-col items-center">
           <p className="bg-gradient-to-b from-neutral-200 to-primary bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl text-center">
             What are you waiting for? <br />
-            <span className="text-lg md:text-3xl text-foreground/60">join now and start connecting with your people!</span>
+            <span className="text-lg md:text-3xl text-foreground/60">
+              join now and start connecting with your people!
+            </span>
           </p>
-          <Button className="p-5 uppercase tracking-widest bg-transparent border border-primary backdrop-blur-sm text-primary hover:text-white">Join Our Community</Button>
+          <Button className="p-5 uppercase tracking-widest bg-transparent border border-primary backdrop-blur-sm text-primary hover:text-white">
+            Join Our Community
+          </Button>
         </div>
       </section>
 
       <footer className="flex justify-center items-center border-t">
-        <p className="text-neutral-600">© {new Date().getFullYear()} VIBEE, made by Daniel Evan</p>
+        <p className="text-neutral-600">
+          © {new Date().getFullYear()} VIBEE, made by Daniel Evan
+        </p>
       </footer>
     </div>
   );
